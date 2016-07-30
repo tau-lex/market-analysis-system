f881bad58a62d3c79cc7b32d1fd85d5f0a63954b
Aleksey Terentev <terentjew.alexey@ya.ru>
2016-07-30 17:07:30 +0300

    /upd config

--- Data base/masdb.lua
+++ Data base/masdb.lua
@@ -3,63 +3,60 @@ box.cfg{
 	--------------------
     -- Basic parameters
     --------------------
-	
 	work_dir = "/home/mint/tarantool_masdb";
 	wal_dir = "/var/log/tarantool";
 	snap_dir = "snap";
 	sophia_dir = "sophia";
-	listen = 3303;
+	listen = 3313;
 	custom_proc_title = 'masdb';
 	pid_file = "masdb.pid";
 	-------------------------
 	-- Storage configuration
 	-------------------------
-	
 	slab_alloc_arena = 1.5;
 --	slab_alloc_minimal = 16;
 --	slab_alloc_maximal = 1048576;
 	slab_alloc_factor = 1.06;
-	
 	-------------------
     -- Snapshot daemon
     -------------------
-
 --	snapshot_period = 0;
 	snapshot_count = 10;
-	
 	-----------
     -- Logging
     -----------
-	
 --	log_level = 5;
 	logger = "masdb.log";
-	
 	--------------------------------
     -- Binary logging and snapshots
     --------------------------------
-	
 --	panic_on_snap_error;
 --	panic_on_wal_error;
 --	rows_per_wal;
 --	snap_io_rate_limit;
 --	wal_mode;
 --	wal_dir_rescan_delay
-	
-	---------------
-    -- Replication
-    ---------------
-	
-	--------------
-    -- Networking
-    --------------
-
 }
 
+if masdb ~= nil then
+	-- Код горячего перезапуска с помощью tarantoolctl или dofile()
+
+	-- Выгрузить старое приложение
+	masdb.stop()
+	-- Очистить кэш для загруженных модулей и зависимостей
+	package.loaded['masdb'] = nil
+	--package.loaded['somedep'] = nil; -- зависимости 'masdb'
+end
+
+-- Загрузить новую версию приложения и все зависимости
+masdb = require('masdb').start()
+
+-- Начальная загрузка()
 local function bootstrap()
-    -- local space = box.schema.create_space('example')
-    -- space:create_index('primary')
+    local space = box.schema.create_space('test')
+    space:create_index('primary')
     -- Закомментируйте это, если вам нужно разграниченный контроль доступа (без него, гость будет иметь доступ ко всему)
-    -- box.schema.user.grant('guest', 'read,write,execute', 'universe')
+    box.schema.user.grant('guest', 'read,write,execute', 'universe')
 
     -- Держите вещи безопасными по умолчанию
     --  box.schema.user.create('example', { password = 'secret' })
@@ -68,4 +65,4 @@ local function bootstrap()
 end
 
 -- для первого запуска создать пространство и добавить установленные гранты
-box.once('example-1.0', bootstrap)
+box.once('testrun-1.0', bootstrap)
