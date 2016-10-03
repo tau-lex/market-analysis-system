eb86a470865ebfbe843cb438a6475e139c0fd24e
Aleksey Terentev <terentjew.alexey@ya.ru>
2016-10-03 10:27:43 +0300

    update

--- Control panel/Debug/doc/CMakeFiles/sphinx-html.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sphinx-html"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sphinx-html.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
