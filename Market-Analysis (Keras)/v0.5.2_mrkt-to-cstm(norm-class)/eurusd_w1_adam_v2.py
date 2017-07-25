0407fb41bc5edc171ca33e273d06b333a07632c5
Terentew Aleksey <terentjew.alexey@ya.ru>
2017-07-25 18:38:23 +0300

    Classification with softmax
    Added a version of the softmax classification file.
    Updated the classification file tanh.

--- Market-Analysis (Keras)/v0.5.2_mrkt-to-cstm(norm-class)/eurusd_w1_adam_v2.py
+++ Market-Analysis (Keras)/v0.5.2_mrkt-to-cstm(norm-class)/eurusd_w1_adam_v2.py
@@ -17,7 +17,7 @@ import numpy as np
 import math
 from keras.backend import backend
 from keras.models import Sequential, model_from_json
-from keras.layers import Dense, GRU, Reshape, Dropout
+from keras.layers import Dense, GRU, Reshape, Dropout, Activation
 from keras.layers import normalization, LeakyReLU
 from keras.callbacks import ReduceLROnPlateau
 from keras import regularizers
@@ -43,27 +43,37 @@ np.random.seed(7)
 
 print( 'Backend =', backend() )
 print( workfile )
-
-
 #=============================================================================#
 #       L O A D   D A T A                                                     #
 #=============================================================================#
 print( 'Prepare Data...' )
 
 data_x = np.genfromtxt( file_x, delimiter=';' )
-data_y = np.genfromtxt( file_y, delimiter=';' )
+data_yt = np.genfromtxt( file_y, delimiter=';' )
 
 # (batch_size, timesteps, units)
 data_x = np.reshape( data_x, (data_x.shape[0], 1, data_x.shape[1]) )
 
 print( "data_x:", data_x.shape )
-print( "data_y:", data_y.shape )
+print( "data_y:", data_yt.shape )
+
+data_y = np.array( [], ndmin=2 )
+for item in data_yt:
+#    print(  )
+    if item > 0:
+        data_y = np.append( data_y, [abs(item), 0.0] )
+    if item < 0:
+        data_y = np.append( data_y, [0.0, abs(item)] )
+    if item == 0:
+        data_y = np.append( data_y, [0.0, 0.0] )
+
+data_y = np.reshape( data_y, (data_yt.shape[0], 2) )
 
 train_size = int( len(data_x) * 0.8 )
 test_size = len(data_x) - train_size
 train_x, test_x = data_x[0:train_size,:], data_x[train_size:len(data_x),:]
 print( len(train_x), len(test_x) )
-train_y, test_y = data_y[0:train_size,], data_y[train_size:len(data_y),]
+train_y, test_y = data_y[0:train_size,:], data_y[train_size:len(data_y),:]
 print( len(train_y), len(test_y) )
 
 
@@ -79,7 +89,8 @@ model.add( LeakyReLU() )
 #    model.add( Dropout( 0.2 ) )
 #    model.add( Reshape( (1, gru1) ) )
 #    model.add( GRU( gru2 ) )
-model.add( Dense( 1, activation='tanh' ) ) # tanh, sigmoid
+model.add( Dense(2) )
+model.add( Activation('softmax') )
 
 reduce_lr = ReduceLROnPlateau( monitor='val_loss', factor=0.9, patience=5, min_lr=0.000001, verbose=1 )
 
@@ -112,11 +123,13 @@ print( "data_xx:", data_xx.shape )
 
 predicted_output = model.predict( data_xx, batch_size=batch_size )
 
-np.savetxt( file_yy, predicted_output, fmt='%.6f', delimiter=';' )
+data_yy = np.array( [] )
+for item in predicted_output:
+    data_yy = np.append( data_yy, item[0] - item[1] )
+
+np.savetxt( file_yy, data_yy, fmt='%.6f', delimiter=';' )
 
 print( "Predict saved:", file_yy )
-
-
 #=============================================================================#
 #       Plot                                                                  #
 #=============================================================================#
@@ -130,7 +143,7 @@ testScore = math.sqrt( mean_squared_error( test_y, testPredict ) )
 print( 'Test Score: %.6f RMSE' % ( testScore ) )
 
 
-plt.plot( predicted_output )
+plt.plot( data_yy )
 plt.title( 'Predicted' )
 plt.ylabel( 'direction')
 plt.xlabel( 'bar')
