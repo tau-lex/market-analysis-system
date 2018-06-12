bdfc8d6c933ac7c75277d749f58eeb0347c59b9c
Terentyev Aleksey <terentjew.alexey@ya.ru>
2018-06-12 18:33:39 +0300

    update;

--- mas_tools/models.py
+++ mas_tools/models.py
@@ -3,8 +3,8 @@
 The module contains functions for working with the Keras models of the MAS project.
 """
 
-from keras.models import Model, model_from_json
-from keras.models import Sequential
+from keras.models import Model, Sequential
+from keras.models import model_from_json
 from keras.layers import Dense, Activation
 from keras.layers import Flatten, BatchNormalization, Reshape
 from keras.layers import LSTM
@@ -22,8 +22,6 @@ def save_model(model: Model, filename: str):
 def load_model(filename: str):
     """Loads the model from a text file."""
 
-    # GOTO: load models from models lib
-    # 'model/' + filename
     json_string = ''
     try:
         file = open(filename, 'r')
@@ -44,6 +42,22 @@ def simple_model(input_shape, nb_output, act='linear'):
     model.add(Reshape((input_shape[0], input_shape[1]), batch_input_shape=(None, 1, input_shape[0], input_shape[1])))
     model.add(BatchNormalization())
     # model.add(BatchNormalization(batch_input_shape=(None, input_shape[0], input_shape[1])))
+    model.add(LSTM(16))
+    model.add(Activation('relu'))
+    model.add(Dense(16))
+    model.add(Activation('relu'))
+    model.add(Dense(16))
+    model.add(Activation('relu'))
+    model.add(Dense(nb_output, activation=act))
+
+    return model
+
+
+def simple_model2(input_shape, nb_output, act='linear'):
+    """Simple model for RL."""
+
+    model = Sequential()
+    model.add(BatchNormalization(batch_input_shape=(None, input_shape)))
     model.add(LSTM(16))
     model.add(Activation('relu'))
     model.add(Dense(16))
