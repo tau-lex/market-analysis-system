539083a7c7ed49934fcbb7cacc9ca3665606fb73
Terentyev Aleksey <terentjew.alexey@ya.ru>
2018-08-12 20:11:27 +0300

    fix;

--- data/to_normalized.py
+++ data/to_normalized.py
@@ -10,10 +10,10 @@ spath = 'E:/Projects/market-analysis-system/data/normalized/'
 
 for symbol in FILES:
     for tf in PERIODS:
-        # tf = '240'
         data = np.genfromtxt(lpath+symbol+tf+CSV, delimiter=';')
         data = data[:, 1:5]
-        data /= np.max(np.abs(data), axis=0)
+        dmax = max(np.max(data[:, 0]), np.max(data[:, 1]), np.max(data[:, 2]), np.max(data[:, 3]))
+        data /= dmax
         np.savetxt(spath+symbol+tf+CSV, data, fmt='%0.6f', delimiter=';')
         data = []
 
