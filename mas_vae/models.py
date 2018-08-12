3ffc8b498c98f04cb1e247aa3ce6fa9a1298cc97
Terentyev Aleksey <terentjew.alexey@ya.ru>
2018-08-12 20:12:50 +0300

    worked vae;

--- mas_vae/models.py
+++ mas_vae/models.py
@@ -1,48 +1,78 @@
 from keras import backend as K
 from keras.models import Model, Sequential
-from keras.layers import Input, Dense, concatenate
+from keras.layers import Input, Dense, concatenate, Lambda
 from keras.layers import Flatten, Reshape, BatchNormalization
 from keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose
+from keras.losses import mse, mae, mape, binary_crossentropy
+from keras import backend as K
 
 from mas_tools.ml import save_model_arch
 
 
-def deep_conv2d_ae(input_shape):
+# reparameterization trick
+# instead of sampling from Q(z|X), sample eps = N(0,I)
+# then z = z_mean + sqrt(var)*eps
+def sampling(args):
+    """Reparameterization trick by sampling fr an isotropic unit Gaussian.
+    Arguments
+        args (tensor): mean and log of variance of Q(z|X)
+    Returns
+        z (tensor): sampled latent vector
+    """
 
-    latent_dim = 32
+    z_mean, z_log_var = args
+    batch = K.shape(z_mean)[0]
+    dim = K.int_shape(z_mean)[1]
+    # by default, random_normal has mean=0 and std=1.0
+    epsilon = K.random_normal(shape=(batch, dim))
+    return z_mean + K.exp(0.5 * z_log_var) * epsilon
+
+
+def deep_conv2d_vae(input_shape):
+    """"""
+    latent_dim = 100
     kernel_size = (1, 5)
     kernel_pooling = (1, 2)
     strides = (1, 1)
     
     # Encoder
     input_tensor = Input(shape=input_shape, name='encoder_input')
+    if len(input_shape) == 2:
+        x = Reshape((input_shape[0], input_shape[1], 1),)(input_tensor)
+    elif len(input_shape) == 3:
+        x = input_tensor
     x = Conv2D(filters=32,
                 kernel_size=kernel_size,
                 padding='same',
                 activation='relu',
-                strides=strides,
-                input_shape=input_shape)(input_tensor)
-    x = Conv2D(filters=64,
-                kernel_size=kernel_size,
-                padding='same',
-                activation='relu',
                 strides=strides)(x)
+    # x = Conv2D(filters=64,
+    #             kernel_size=kernel_size,
+    #             padding='same',
+    #             activation='relu',
+    #             strides=strides)(x)
     # shape info needed to build decoder model
     shape = K.int_shape(x)
     # shape = enc.output_shape
     # generate latent vector Q(z|X)
     x = Flatten()(x)
-    x = Dense(latent_dim, activation='relu', name='encoder_output')(x)
+    x = Dense(100, activation='relu', name='encoder_output')(x)
+
+    z_mean = Dense(latent_dim, name='z_mean')(x)
+    z_log_var = Dense(latent_dim, name='z_log_var')(x)
+    # use reparameterization trick to push the sampling out as input
+    # note that "output_shape" isn't necessary with the TensorFlow backend
+    z = Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
 
     # Decoder
     latent_inputs = Input(shape=(latent_dim,), name='latent_input')
     y = Dense(shape[1] * shape[2] * shape[3], activation='relu')(latent_inputs)
     y = Reshape((shape[1], shape[2], shape[3]))(y)
-    y = Conv2DTranspose(filters=64,
-                    kernel_size=kernel_size,
-                    padding='same',
-                    activation='relu',
-                    strides=strides)(y)
+    # y = Conv2DTranspose(filters=64,
+    #                 kernel_size=kernel_size,
+    #                 padding='same',
+    #                 activation='relu',
+    #                 strides=strides)(y)
     y = Conv2DTranspose(filters=32,
                     kernel_size=kernel_size,
                     padding='same',
@@ -54,23 +84,37 @@ def deep_conv2d_ae(input_shape):
                     activation='relu',
                     strides=strides,
                     name='decoder_output')(y)
+    if len(input_shape) == 2:
+        output = Reshape((input_shape[0], input_shape[1]))(y)
+    elif len(input_shape) == 3:
+        output = Reshape(input_shape)(y)
 
     # Create models
-    encoder = Model(input_tensor, x, name='encoder')
-    decoder = Model(latent_inputs, y, name='decoder')
-    autoencoder = Model(input_tensor, decoder(encoder(input_tensor)), name='ae')
+    encoder = Model(input_tensor, [z_mean, z_log_var, z], name='encoder')
+    decoder = Model(latent_inputs, output, name='decoder')
+    autoencoder = Model(input_tensor, decoder(encoder(input_tensor)[2]), name='ae')
 
-    return encoder, decoder, autoencoder
+    # VAE loss = mse_loss or xent_loss + kl_loss
+    if True:
+        reconstruction_loss = mse(K.flatten(input_tensor), K.flatten(output))
+    else:
+        reconstruction_loss = binary_crossentropy(K.flatten(input_tensor), K.flatten(outputs))
+    reconstruction_loss *= (input_shape[0] * input_shape[1])
+    kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
+    kl_loss = K.sum(kl_loss, axis=-1)
+    kl_loss *= -0.5
+    vae_loss = K.mean(reconstruction_loss + kl_loss)
+
+    return encoder, decoder, autoencoder, vae_loss
 
 
 if __name__ == "__main__":
     path = 'E:/Projects/market-analysis-system/'
 
-    enc, dec, ae = deep_conv2d_ae((4, 20, 1))
+    enc, dec, ae, _ = deep_conv2d_vae((4, 20))
     save_model_arch(enc, path+'ae_enc')
     enc.summary()
     save_model_arch(dec, path+'ae_dec')
     dec.summary()
     save_model_arch(ae, path+'ae')
     ae.summary()
-    
--- mas_vae/train_autoencoders.py
+++ mas_vae/train_autoencoders.py
@@ -2,75 +2,71 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-from mas_tools.data import create_timeseries_matrix
-import mas_tools.layers.autoencoders as vae
+# import mas_tools.models.autoencoders as vae
+from models import deep_conv2d_vae
+
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 
 from sklearn.model_selection import train_test_split
 from mas_tools.ml import plot_history
 
 wpath = 'E:/Projects/market-analysis-system/mas_vae/'
-path = 'E:/Projects/market-analysis-system/data/transformed/'
-files = ['EURUSD15.csv', 'AUDJPY15.csv', 'AUDUSD15.csv', 'CHFJPY15.csv',
-        'EURAUD15.csv', 'EURCAD15.csv', 'EURCHF15.csv', 'EURGBP15.csv',
-        'EURJPY15.csv', 'EURRUB15.csv']
-# files = []
+lpath = 'E:/Projects/market-analysis-system/data/windowed/'
 
-# Load Data
-# file_x = path + files[0]
-# data_x = np.genfromtxt(file_x, delimiter=';')[:, 1:5]
-# data_x, _ = create_timeseries_matrix(data_x, look_back=20)
-# data_x = np.reshape(data_x, (len(data_x), 20, 4))
+symbols = ['GBPCHF', 'GBPJPY', 'GBPUSD', 'NZDJPY']
+window = 20
+prefix = 'norm_w20'
+tf = '240.csv'
+model_name = 'conv_ae_20_'
 
-data_x = np.ones((80,), dtype=np.float)
-data_x = np.reshape(data_x, (1, 20, 4))
+data = np.ones((window,), dtype=np.float)
+data = np.reshape(data, (1, window))
 
 start_t = time.clock()
-for fn in files:
-    new_data = np.genfromtxt(path+fn, delimiter=';')[:, 1:5]
-    new_data, _ = create_timeseries_matrix(new_data, look_back=20)
-    new_data = np.reshape(new_data, (len(new_data), 20, 4))
-    data_x = np.vstack((data_x, new_data))
+for symbol in symbols:
+    new_data = np.genfromtxt(lpath+prefix+symbol+tf, delimiter=';')
+    new_data = np.reshape(new_data, (len(new_data), window, 4))
+    new_data = np.reshape(new_data[:, :, 3], (len(new_data), window))
+    data = np.vstack((data, new_data))
+data = np.reshape(data[1:, :], (len(data)-1, 1, window))
+x_train, x_test = train_test_split(data, shuffle=True, test_size=0.1)
+data = None # Clear memory
 
-print('New data shape: {} \nRead+Calc time: {}'.format(data_x.shape, time.clock()-start_t))
-
-x_train, x_test = train_test_split(data_x, test_size=0.1)
-data_x = None # Clear memory
+print('New data shape: {} \nRead+Calc time: {}'.format(x_train.shape, time.clock()-start_t))
 
 
 # Train AE
-encoder, decoder, autoencoder = vae.create_deep_ae((20, 4), 64)
+encoder, decoder, autoencoder, loss = deep_conv2d_vae((1, window))
 
-autoencoder.compile(optimizer='nadam', loss='mse')
+autoencoder.compile(optimizer='adam', loss='mape', metrics=['acc'])
 
-autoencoder.load_weights(wpath+'ae_deep_20-4_64.hdf5', by_name=True)
+# autoencoder.load_weights(wpath+model_name+'_ended.hdf5', by_name=True)
 
 reduce_lr = ReduceLROnPlateau(factor=0.1, patience=3, min_lr=0.00001, verbose=1)
-checkpointer = ModelCheckpoint(wpath+'ae_deep_{epoch:02d}-{val_loss:.2f}.hdf5', verbose=0, save_best_only=True)
+checkpointer = ModelCheckpoint(wpath+model_name+'{epoch:02d}-{val_loss:.2f}.hdf5', verbose=0, save_best_only=True)
 
 history = autoencoder.fit(x_train, x_train,
                             epochs=10,
-                            batch_size=512,
+                            batch_size=1024,
                             shuffle=True,
                             callbacks=[checkpointer, reduce_lr],
                             validation_data=(x_test, x_test))
 
-autoencoder.save_weights(wpath+'ae_deep_20-4_64.hdf5')
-
+autoencoder.save_weights(wpath+model_name+'_ended.hdf5')
 
 # View
 plot_history(history, acc='acc')
 
-nb = [3000, 15000, 29000]
 
-for n in nb:
-    code = encoder.predict(x_test[n].reshape((1, 20, 4)), batch_size=1)
-    y_test = decoder.predict(code, batch_size=1)
+for n in [3000, 1500, 9000]:
+    code = encoder.predict(x_test[n].reshape((1, 1, window)), batch_size=1)
+    y_test = decoder.predict(code[2], batch_size=1)
 
-    plt.plot(x_test[n, :, 0])
-    plt.plot(y_test[0, :, 0])
-    plt.plot(x_test[n, :, 3])
-    plt.plot(y_test[0, :, 3])
+    # plt.plot(x_test[n, 0, :])
+    # plt.plot(y_test[0, 0, :])
+    print(code[2])
+    plt.plot(x_test[n, 0, :])
+    plt.plot(y_test[0, 0, :])
     plt.ylabel('price')
     plt.xlabel('bar')
     plt.legend(['open_true', 'open_decoded', 'close_true', 'close_decoded'])
