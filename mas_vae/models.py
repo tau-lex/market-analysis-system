b4d9b1dd794d57c394df0a69a2f3074011cccd7c
Terentyev Aleksey <terentjew.alexey@ya.ru>
2018-09-19 23:21:31 +0300

    added dropout and initializer;

--- mas_vae/models.py
+++ mas_vae/models.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from keras import backend as K
 from keras.models import Model, Sequential
 from keras.layers import Input, Dense, concatenate, Lambda
-from keras.layers import Flatten, Reshape, BatchNormalization
+from keras.layers import Flatten, Reshape, BatchNormalization, Dropout
 from keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose, UpSampling2D
 from keras.losses import mse, mae, mape, binary_crossentropy
 from keras import backend as K
@@ -31,74 +31,14 @@ def sampling(args):
     return z_mean + K.exp(0.5 * z_log_var) * epsilon
 
 
-def plot_results(models,
-                 data,
-                 batch_size=128,
-                 w=20,
-                 model_name="vae"):
-    """Plots labels and MNIST digits as function of 2-dim latent vector
-    # Arguments:
-        models (tuple): encoder and decoder models
-        data (tuple): test data and label
-        batch_size (int): prediction batch size
-        model_name (string): which model is using this function
-    """
-
-    encoder, decoder = models
-    x_test, y_test = data
-    os.makedirs(model_name, exist_ok=True)
-
-    filename = os.path.join(model_name, "vae_mean.png")
-    # display a 2D plot of the digit classes in the latent space
-    z_mean, _, _ = encoder.predict(x_test,
-                                   batch_size=batch_size)
-    plt.figure(figsize=(12, 10))
-    plt.scatter(z_mean[:, 0], z_mean[:, 1], c=y_test)
-    plt.colorbar()
-    plt.xlabel("z[0]")
-    plt.ylabel("z[1]")
-    plt.savefig(filename)
-    plt.show()
-
-    filename = os.path.join(model_name, "digits_over_latent.png")
-    # display a 2D manifold of pictures
-    n = w * 4 + 2
-    digit_size = w * 4
-    figure = np.zeros((digit_size * n, digit_size * n))
-    # linearly spaced coordinates corresponding to the 2D plot
-    # of digit classes in the latent space
-    grid_x = np.linspace(-4, 4, n)
-    grid_y = np.linspace(-4, 4, n)[::-1]
-
-    for i, yi in enumerate(grid_y):
-        for j, xi in enumerate(grid_x):
-            z_sample = np.array([[xi, yi]])
-            x_decoded = decoder.predict(z_sample)
-            digit = x_decoded[0].reshape(digit_size, digit_size, 3)
-            figure[i * digit_size: (i + 1) * digit_size,
-                   j * digit_size: (j + 1) * digit_size] = digit
-
-    plt.figure(figsize=(10, 10))
-    start_range = digit_size // 2
-    end_range = n * digit_size + start_range + 1
-    pixel_range = np.arange(start_range, end_range, digit_size)
-    sample_range_x = np.round(grid_x, 1)
-    sample_range_y = np.round(grid_y, 1)
-    plt.xticks(pixel_range, sample_range_x)
-    plt.yticks(pixel_range, sample_range_y)
-    plt.xlabel("z[0]")
-    plt.ylabel("z[1]")
-    plt.imshow(figure, cmap='Greys_r')
-    plt.savefig(filename)
-    plt.show()
-
-
-def deep_conv2d_vae(input_shape):
+def deep_conv2d_vae(input_shape,
+                    latent_dim = 160):
     """"""
-    latent_dim = 100
     kernel_size = (3, 3)
     kernel_pooling = (2, 2)
     strides = (1, 1)
+    dropout = 0.5
+    initializer = 'truncated_normal'
     
     # Encoder
     input_tensor = Input(shape=input_shape, name='encoder_input')
@@ -106,18 +46,22 @@ def deep_conv2d_vae(input_shape):
         x = Reshape((input_shape[0], input_shape[1], 1))(input_tensor)
     elif len(input_shape) == 3:
         x = input_tensor
-    x = BatchNormalization()(x)
+    # x = BatchNormalization()(x)
     x = Conv2D(filters=32,
                 kernel_size=kernel_size,
                 padding='same',
                 activation='relu',
+                kernel_initializer=initializer,
                 strides=strides)(x)
+    x = Dropout(dropout)(x)
     x = MaxPooling2D(kernel_pooling)(x)
     x = Conv2D(filters=64,
                 kernel_size=kernel_size,
                 padding='same',
                 activation='relu',
+                kernel_initializer=initializer,
                 strides=strides)(x)
+    x = Dropout(dropout)(x)
     x = MaxPooling2D(kernel_pooling)(x)
     # shape info needed to build decoder model
     shape = K.int_shape(x)
@@ -137,24 +81,29 @@ def deep_conv2d_vae(input_shape):
     y = Dense(shape[1] * shape[2] * shape[3], activation='relu')(latent_inputs)
     y = Reshape((shape[1], shape[2], shape[3]))(y)
     y = UpSampling2D(kernel_pooling)(y)
+    y = Dropout(dropout)(y)
     y = Conv2DTranspose(filters=64,
                     kernel_size=kernel_size,
                     padding='same',
                     activation='relu',
+                    kernel_initializer=initializer,
                     strides=strides)(y)
     y = UpSampling2D(kernel_pooling)(y)
+    y = Dropout(dropout)(y)
     y = Conv2DTranspose(filters=32,
                     kernel_size=kernel_size,
                     padding='same',
                     activation='relu',
+                    kernel_initializer=initializer,
                     strides=strides)(y)
     y = Conv2DTranspose(filters=(1 if len(input_shape) == 2 else 3),
                     kernel_size=kernel_size,
                     padding='same',
                     activation='relu',
+                    kernel_initializer=initializer,
                     strides=strides,
                     name='decoder_output')(y)
-    y = BatchNormalization()(y)
+    # y = BatchNormalization()(y)
     if len(input_shape) == 2:
         output = Reshape((input_shape[0], input_shape[1]))(y)
     elif len(input_shape) == 3:
@@ -165,16 +114,17 @@ def deep_conv2d_vae(input_shape):
     decoder = Model(latent_inputs, output, name='decoder')
     autoencoder = Model(input_tensor, decoder(encoder(input_tensor)[2]), name='ae')
 
-    # VAE loss = mse_loss or xent_loss + kl_loss
-    if True:
-        reconstruction_loss = mse(K.flatten(input_tensor), K.flatten(output))
-    else:
-        reconstruction_loss = binary_crossentropy(K.flatten(input_tensor), K.flatten(outputs))
-    reconstruction_loss *= (input_shape[0] * input_shape[1])
+    reconstruction_loss = mse(K.flatten(input_tensor), K.flatten(output))
+    if len(input_shape) == 2:
+        reconstruction_loss *= (input_shape[0] * input_shape[1])
+    elif len(input_shape) == 3:
+        reconstruction_loss *= (input_shape[0] * input_shape[1] * input_shape[2])
     kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
     kl_loss = K.sum(kl_loss, axis=-1)
     kl_loss *= -0.5
     vae_loss = K.mean(reconstruction_loss + kl_loss)
+    # autoencoder.add_loss(vae_loss)
+    # autoencoder.compile(optimizer='adam', metrics=['acc'])
 
     return encoder, decoder, autoencoder, vae_loss
 
